583be740d264beb414182a223d53ba7b66f555af
Rogelio Zamora Jr <dotenterface@gmail.com>
2023-07-24 08:56:01 -0500

    +User Profile Integration

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -23,14 +23,15 @@ const PostListItem = ({ post }: PostListItemProps) => {
   return (
     <Link href={`/posts/${post.id}`} asChild>
       <Pressable style={styles.container}>
-        <View style={styles.header}>
-          <Image source={{ uri: post.author.image }} style={styles.userImage} />
-          <View>
-            <Text style={styles.userName}>{post.author.name}</Text>
-            <Text style={styles.position}>{post.author.position}</Text>
-          </View>
-        </View>
-
+        <Link href={`/users/${post.author.id}`} asChild>
+          <Pressable style={styles.header}>
+            <Image source={{ uri: post.author.image }} style={styles.userImage} />
+            <View>
+              <Text style={styles.userName}>{post.author.name}</Text>
+              <Text style={styles.position}>{post.author.position}</Text>
+            </View>
+          </Pressable>
+        </Link>
         <Text style={styles.content}>{post.content}</Text>
         {post.image && (
           <Image source={{ uri: post.image }} style={styles.postImage} />
